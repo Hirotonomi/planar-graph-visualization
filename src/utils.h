9c621fbc87d1a3136a3f56e4536b3aea1b230a3d
10v-jan-rekas <jan.rekas@tenvalleys>
2026-03-27 16:30:58 +0100

    Updated utils.h for count_file_lines

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
+#include <stdio.h> //FILE type hmhmhmmmhmhmhmmhmmhmhmmm "insert regular show reference"
 
 int parse_algorithm(const char *name);
+int count_file_lines(FILE *file);
 
 #endif
